e33873897aead76ccee02aa3a74b4ddc41eaebc7
Vũ Hồng Thịnh <toilathinh150503@gmail.com>
2024-12-02 17:36:25 +0700

    commit pos database connect

--- ASP.Net API/POS-System_DAL/Data/OnlinePosContext.cs
+++ ASP.Net API/POS-System_DAL/Data/OnlinePosContext.cs
@@ -60,6 +60,7 @@ public partial class OnlinePosContext : DbContext
     {
         //optionsBuilder.UseSqlServer("Server=16ACH-LEGION;Initial Catalog=POS-Web;Persist Security Info=True;Encrypt=True;TrustServerCertificate=True;Trusted_Connection=true");
         optionsBuilder.UseSqlServer("Server=THINKPAD-X395\\SQLEXPRESS;Database=POS-Web;TrustServerCertificate=True;Trusted_Connection=True;");
+        //optionsBuilder.UseSqlServer("Server=DESKTOP-6J6JQBK\\SQLEXPRESS;Database=POS-Web;TrustServerCertificate=True;Trusted_Connection=True;");
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
