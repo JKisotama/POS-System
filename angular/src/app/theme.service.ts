40b31ee5446ae912761462ab6919bc42fd2310fe
Phạm Quốc Tuấn <Kuuhaku986@gmail.com>
2024-12-18 17:07:55 +0700

    Upodate theme mode

--- angular/src/app/theme.service.ts
+++ angular/src/app/theme.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ThemeService {
-  private darkMode: boolean = false;
+  private darkMode: boolean = true;
 
   isDarkMode(): boolean {
     return this.darkMode;
@@ -13,9 +13,11 @@ export class ThemeService {
   setDarkMode(isDarkMode: boolean): void {
     this.darkMode = isDarkMode;
     if (isDarkMode) {
-      document.body.classList.add('light-mode');
-    } else {
+      document.body.classList.add('dark-mode');
       document.body.classList.remove('light-mode');
+    } else {
+      document.body.classList.add('light-mode');
+      document.body.classList.remove('dark-mode');
     }
   }
 }
