1f12dab5743eb33e3d3722677f4f87aae9061347
Trần Quân <quanTND9707@gmail.com>
2024-12-20 00:18:27 +0700

    update SU

--- ASP.Net API/POS-System_BAL/DTOs/UserDTO.cs
+++ ASP.Net API/POS-System_BAL/DTOs/UserDTO.cs
@@ -10,8 +10,12 @@ namespace POS_System_BAL.DTOs
     public class UserDTO
     {
         public string? FullName { get; set; }
-
         public string? IdentifyString { get; set; }
+        public string? Address { get; set; }
+        public string? Phone { get; set; }
+        public string? DoB { get; set; }
+        public string? Email { get; set; }
+        public string? Gender { get; set; }
         
     }
 }
